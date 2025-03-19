8f243ae8489ff0b1482f819cdafc42580c150325
Bacheron-bullon <francois.staat@gmail.com>
2025-03-19 13:12:52 +0100

    Update mastermind.py

--- mastermind/mastermind.py
+++ mastermind/mastermind.py
@@ -8,22 +8,24 @@ globcode = code1()
 print(globcode)
 
 def devine():
-    bornes = {}
+    bornes = []
     for i in range(4):
-        bornes[i]= int(input("selectionner un chiffre"))
+        bornes.append(int(input("selectionner un chiffre")))
     return bornes
 globbornes = devine()
 print (globbornes)
 
-
 def compare():
-    for i in range(3):
-        Bon_endroit = 0
-        Mauvais_endroit = 0
-        if globbornes[i] == globcode[i]:
+    Bon_endroit = 0
+    Mauvais_endroit = 0
+    for elem1,elem2 in zip(globbornes,globcode):
+        if elem1 == elem2:
             Bon_endroit = Bon_endroit + 1
-        elif globbornes[i] in globcode:
+    
+    for i in range (4):
+        if globbornes[i] in globcode:
             Mauvais_endroit = Mauvais_endroit + 1
+    Mauvais_endroit = Mauvais_endroit - Bon_endroit
     print(Bon_endroit,'chiffres placés au bon endroit et',Mauvais_endroit,'chiffres placés au mauvais endroit')
     return
 compare()
