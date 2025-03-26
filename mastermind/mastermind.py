67d19d64e73e5452a1aebcefb6e9d81ba09c07ff
Bacheron-bullon <francois.staat@gmail.com>
2025-03-26 12:07:42 +0100

    Update mastermind.py

--- mastermind/mastermind.py
+++ mastermind/mastermind.py
@@ -7,31 +7,48 @@ def code1():
 globcode = code1()
 print(globcode)
 
-crack = False 
-while crack == False:
-    def devine():
-        bornes = []
-        for i in range(4):
-            bornes.append(int(input("selectionner un chiffre")))
-        return bornes
-    globbornes = devine()
-    print (globbornes)
 
-    def compare():
+
+def obtenir_chiffres():
+        liste_chiffres = []
+        while len(liste_chiffres) < 4:
+            try:
+                chiffre = int(input("Entrez un chiffre entre 0 et 9 : "))
+                if 0 <= chiffre <= 9:
+                    liste_chiffres.append(chiffre)
+                else:
+                    print("Veuillez entrer un chiffre valide entre 0 et 9.")
+            except ValueError:
+                print("Entrée invalide. Veuillez entrer un chiffre entre 0 et 9.")
+        return liste_chiffres
+
+def compare():
         Bon_endroit = 0
         Mauvais_endroit = 0
-        chiffre = set()
         for elem1,elem2 in zip(globbornes,globcode):
-            chiffre.add(elem1)
             if elem1 == elem2:
                 Bon_endroit = Bon_endroit + 1
-        
-        for i in range (4):
-            if globbornes[i] in chiffre:
+
+        for element in globcode:
+            if element in globbornes:
                 Mauvais_endroit = Mauvais_endroit + 1
-                chiffre.remove(globbornes[i])
         Mauvais_endroit = Mauvais_endroit - Bon_endroit
         print(Bon_endroit,'chiffres placés au bon endroit et',Mauvais_endroit,'chiffres placés au mauvais endroit')
         return Bon_endroit
+
+
+
+
+Historique = []
+crack = False 
+while crack == False:
+    
+    globbornes = obtenir_chiffres()
+    Historique.append(globbornes)
+    for element in Historique:
+        print(element)
+
+    
     if compare() == 4:
         crack = True
+print("Bravo, vous avez trouver le code !!!")
